3d52d85828653b09a8aab122ff8d701a731b4819
Singh, Jay <Jay.Singh@philips.com>
2021-08-08 09:36:20 +0530

    First web app

---
+++ ASP.NET.SampleWebApp/ASP.Net.SamepleWebApp/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace ASP.Net.SamepleWebApp
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ ASP.NET.SampleWebApp/ASP.Net.SamepleWebApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ASP.Net.SamepleWebApp.MvcApplication" Language="C#" %>
